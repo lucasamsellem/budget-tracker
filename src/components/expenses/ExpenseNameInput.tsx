2cc2757c98c412485ff3ed0747b7c741085d1b85
Lucas Amsellem <lucas.amsellem21@gmail.com>
2024-12-06 13:23:34 +0100

    fixed types

--- src/components/expenses/ExpenseNameInput.tsx
+++ src/components/expenses/ExpenseNameInput.tsx
@@ -1,18 +1,22 @@
-import { Expense } from '@/types/Expense'
+import { Expense, OnExpense } from '@/types/Expense'
 import { Input } from '../ui/input'
 
 type ExpenseNameInputProps = {
   expense: Expense
-  onExpenseUpdate: (updatedExpense: Expense) => void
+  onExpense?: OnExpense
+  onExpenseUpdate?: (updatedExpense: Expense) => void
 }
 
-function ExpenseNameInput({ expense, onExpenseUpdate }: ExpenseNameInputProps) {
+function ExpenseNameInput({ expense, onExpense, onExpenseUpdate }: ExpenseNameInputProps) {
   return (
     <Input
       value={expense.name}
       type='text'
       placeholder='Name'
-      onChange={e => onExpenseUpdate({ ...expense, name: e.target.value })}
+      onChange={e => {
+        onExpense?.({ ...expense, name: e.target.value })
+        onExpenseUpdate?.({ ...expense, name: e.target.value })
+      }}
       onClick={e => e.stopPropagation()}
     />
   )
