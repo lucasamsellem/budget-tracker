6d9f5f522945aa3f6b41bcb60a77ed782aa805f5
Lucas Amsellem <lucas.amsellem21@gmail.com>
2024-12-09 13:02:14 +0100

    code refactoring

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -1,26 +1,30 @@
 import * as React from 'react'
 import * as ProgressPrimitive from '@radix-ui/react-progress'
-
 import { cn } from '@/lib/utils'
 
-const Progress = React.forwardRef<
-  React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ barColor, className, value, ...props }, ref) => (
-  <ProgressPrimitive.Root
-    ref={ref}
-    className={cn(
-      'relative h-2 w-full overflow-hidden rounded-full bg-zinc-900/20 dark:bg-zinc-50/20',
-      className
-    )}
-    {...props}
-  >
-    <ProgressPrimitive.Indicator
-      className={`h-full w-full flex-1 transition-all`}
-      style={{ transform: `translateX(-${100 - (value || 0)}%)`, backgroundColor: barColor }}
-    />
-  </ProgressPrimitive.Root>
-))
+// Extend the ProgressPrimitive types to include `barColor`
+interface ProgressProps extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {
+  barColor?: string // Add barColor as an optional prop
+}
+
+const Progress = React.forwardRef<React.ElementRef<typeof ProgressPrimitive.Root>, ProgressProps>(
+  ({ barColor, className, value, ...props }, ref) => (
+    <ProgressPrimitive.Root
+      ref={ref}
+      className={cn(
+        'relative h-2 w-full overflow-hidden rounded-full bg-zinc-900/20 dark:bg-zinc-50/20',
+        className
+      )}
+      {...props}
+    >
+      <ProgressPrimitive.Indicator
+        className={`h-full w-full flex-1 transition-all`}
+        style={{ transform: `translateX(-${100 - (value || 0)}%)`, backgroundColor: barColor }}
+      />
+    </ProgressPrimitive.Root>
+  )
+)
+
 Progress.displayName = ProgressPrimitive.Root.displayName
 
 export { Progress }
